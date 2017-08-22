9c83a8833c7e6cd99fbb47c0c6dc5ec473b7d191
Tariqul Isam <leg3ndtariq@gmail.com>
2017-08-22 15:51:37 +0600

    Update Universe.php

--- php-template/Universe.php
+++ php-template/Universe.php
@@ -1,9 +1,3 @@
 <?php
-    function countAllStars($galaxies) {
-        $totalStars = 0;
-        foreach ($galaxies as $star) {
-            $totalStars = $star; //Hint: Change = to +=
-        }
-        return $totalStars;
-    }
+    //write your code
 ?>
